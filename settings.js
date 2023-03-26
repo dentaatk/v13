0860fe148f5cb9e6ec8575de7229a5a4858560da
dentaatk <dentakuncoro9898@gmail.com>
2023-03-27 00:49:22 +0900

    apaya

---
+++ settings.js
@@ -0,0 +1,252 @@
+const fs = require('fs')
+
+// APIKEY OPEN AI
+global.keyopenai  = "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" // ISI APIKEY LU
+
+// Api
+global.APIs = {
+	alfa: 'https://api.zeeoneofc.my.id',
+}
+
+// APIKEY \\
+global.APIKeys = {
+	'https://api.zeeoneofc.my.id': 'jerapikeytod',
+}
+
+global.namabot = "EBOT" // UBAH JADI NAMA LU
+global.namaowner = "GOMBLOH" // NAMA OWNER
+global.footer_text = "© EBOT" // NAMA BOT
+global.pp_bot = fs.readFileSync("./image/ppbot.jpg") // FOTO BOT MAX 50KB BIAR GA DELAY
+global.qris = fs.readFileSync("./image/qris.jpg") // FOTO QRIS MAX 50KB BIAR GA DELLAY
+global.owner = ['6281210944252','6285240184110'] // UBAH NOMOR YANG MAU DI JADIKAN OWNER
+// PEMISAH
+global.sessionName = 'session' // GAK USAH UBAH
+global.prefa = ['', '!', '.', '🐦', '🐤', '🗿'] // GAK USAH UBAH
+global.sewabot = "CHAT OWNER UNTUK HARGA NYA" // ISI HARGA SEWA BOT LU
+global.donasi = "DONASI NYA KAK BIAR BOT NYA BISA AKTIF TERUS" // QRIS SAMA FOTO DONASI UDAH CONNECT TINGGAL GANTI FOTO QRIS AJA
+global.script = ("_") // BEBAS ASAL JAN HAPUS
+global.websitex = ("_") // ISI BEBAS ASAL JAN HAPUS
+global.myweb = "_" // bebas asal jan hapus
+
+
+// NAME STICKER
+global.packname = 'By' //sticker wm ubah
+global.author = 'GOMBLOH' //sticker wm ganti nama kalian
+// UBAH AJA NAMA NYA 
+
+// BUY NO ENC 70K CHAT ME
+https://wa.me/6285718828566
+
+module.exports.helpMenu = (pushname) =>{
+	return `Halo Kak 👋 ${pushname}
+Saya ${namabot}, Saya Adalah Bot WhatsApp .
+
+╭✄┈┈┈⟬ *INFO-BOT* ⟭
+┆❐ Creator : ${namaowner}
+┆❐ Bot Name : ${namabot}
+╰──────────◇
+___________________________
+
+╭─❒ 「 FREE MENU 」
+│
+│○ #owner 
+│○ #sc 
+│○ #ping 
+│○ #donate 
+│○ #tqto 
+│○ #jadibot 
+│○ #sewa  
+│○ #jasrun 
+│○ #sewa 
+│○ #buyprem 
+│
+╰❒ 
+
+╭─❒ 「 OWNER MENU 」
+│
+│○ !addprem
+│○ !delprem 
+│○ !listprem
+│○ !block 
+│○ !unblock 
+│○ !public 
+│○ !self 
+│○ !out 
+│○ !kosong 
+│○ !join 
+│○ !addsewa 
+│○ !delsewa 
+│○ !setnamabot 
+│○ !setbiobot 
+│○ !broadcastvid 
+│○ !bctext 
+│○ !bcimage 
+│
+╰❒
+
+╭─❒ 「 Ahha YAMETE 」 
+│
+│○ !runtime 
+│○ !groupbot 
+│○ !developer 
+│○ !gitclone 
+│
+╰❒
+
+╭─❒ 「  STORE MENU  」
+│
+│○ !list 
+│○ !addlist 
+│○ !updatelist 
+│○ !dellist 
+│○ !jeda 
+│○ !tambah 
+│○ !kurang
+│○ !kali 
+│○ !bagi 
+│○ !setproses 
+│○ !changeproses 
+│○ !delsetproses 
+│○ !setdone 
+│○ !changedone 
+│○ !delsetdone 
+│○ !proses 
+│○ !done 
+│○ !setwelcome 
+│○ !changewelcome 
+│○ !delsetwelcome 
+│○ !setleft 
+│○ !changeleft 
+│○ !delsetleft 
+│
+╰❒
+
+╭─❒ 「 GROUP MENU 」 
+│
+│○ !antiwame 
+│○ !antiwame2 
+│○ !antilink 
+│○ !antilink2 
+│○ !welcome 
+│○ !goodbye 
+│○ !open 
+│○ !close 
+│○ !hidetag 
+│○ !kick 
+│○ !linkgc 
+│○ !resetlinkgc 
+│○ !delete 
+│○ !listonline 
+│○ !setnamegc 
+│○ !setdesc 
+│○ !listsewa 
+│○ !add 
+│○ !promote
+│○ !demote 
+│○ !antilinkall 
+│○ !antitoxic 
+│○ !sendlinkgc 
+│○ !tagall 
+│
+╰❒
+
+╭─❒ 「 SEARCH MENU 」 
+│
+│○ !ytsearch 
+│○ !google 
+│
+╰❒
+
+╭─❒ 「 DOWNLOAD MENU 」
+│ 
+│○ !tiktok 
+│○ !tiktokvn
+│○ !tiktokaudio 
+│○ !spotify 
+│○ !ytmp3 
+│○ !ytmp4 
+│○ !play 
+│○ !playmp3 
+│○ !playmp4 
+│○ !lyrics 
+│○ !igdl 
+│○ !igvideo 
+│
+╰❒
+
+╭─❒ 「 ASUPAN MENU 」
+│
+│○ !tiktokkayes 
+│○ !tiktokgirl 
+│○ !tiktokghea 
+│○ !tiktokbocil 
+│○ !tiktokukhty 
+│○ !tiktoksantuy 
+│○ !tiktokpanrika 
+│○ !tiktoknotnot 
+│○ !tiktokgabagtha 
+│
+╰❒
+
+╭─❒ 「 RANDOM STICKER 」 
+│
+│○ !patrick
+│○ !popoci
+│○ !sponsbob
+│○ !kawan-sponsbob
+│○ !awoawo
+│○ !chat
+│○ !benedict
+│○ !dbfly
+│○ !dino-kuning
+│○ !doge
+│○ !gojosatoru
+│○ !hope-boy
+│○ !jisoo
+│○ !kr-robot
+│○ !kucing
+│○ !lonte
+│○ !manusia_lidi
+│○ !menjamet
+│○ !meow
+│○ !nicholas
+│○ !tyni
+│
+╰❒
+
+╭─❒ 「 MAIN MENU 」 
+│
+│○ !sticker 
+│○ !toimage 
+│○ !pinterest 
+│○ !ttp 
+│○ !smeme 
+│○ !attp 
+│○ !emojimix 
+│○ !wm 
+│
+╰❒
+
+╭─❒ 「 Anonymous MENU 」 
+│
+│○ !menfess 
+│
+╰❒
+
+╭─❒ 「 Ai Menu 」 
+│
+│○ !ai 
+│○ !chatgpt 
+│
+╰❒
+
+╭─❒ 「 STALKER MENU 」 
+│
+│○ !ffstalk 
+│○ !mlstalk 
+│
+╰❒
+
+📝 *NOTE:BOT INI MASIH DALAM PENGEMBANGAN*
+`
+}
